8699115b50a50d70bda34122d1c00555d092426f
Faith <faithhafen@gmail.com>
2020-08-13 11:58:37 -0600

    added vendor prefixes to animation and fixed protected route

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import MyCats from './components/MyCats'
 import Navbar from './components/Navbar'
 import NoMatch from './components/NoMatch'
 import Register from './components/Register'
-import { Switch, Route, } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import FetchUser from './components/FetchUser'
 import ProtectedRoute from './components/ProtectedRoute'
 
@@ -14,7 +14,7 @@ const App = () => (
     <Navbar />
     <FetchUser>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <ProtectedRoute exact path="/" component={Home} />
         <ProtectedRoute exact path='/my_cats' component={MyCats} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
